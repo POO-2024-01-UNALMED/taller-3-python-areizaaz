8d323eb2433bcce1ea201fb1509e5a5976c31ce1
areizaaz <joareizag@unal.edu.co>
2024-02-25 21:03:51 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -1,7 +1,7 @@
 class Control:
 
-    def __init__(self,tv) -> None:
-         self._tv = tv
+    def __init__(self) -> None:
+         self._tv = None
 
     def turnOn(self):
         self._tv.turnOn()
