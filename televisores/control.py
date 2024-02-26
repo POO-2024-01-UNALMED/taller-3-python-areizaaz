c214335ec6397c5a347676f9a10b130000868438
areizaaz <joareizag@unal.edu.co>
2024-02-25 22:11:46 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -17,12 +17,10 @@ class Control:
     def volumenDown(self):
         self._tv.volumenDown()
 
-    def setCanal(self, canal):
-        if(canal >= 1 and canal <= 120):
-            self._canal = canal
-    def setVolumen(self, volumen):
-        if(volumen >= 0 and volumen <= 7):
-            self._volumen = volumen
+    def setCanal(self):
+        self._tv.setCanal()
+    def setVolumen(self):
+        self._tv.setVolumen()
     
     def setTv(self,tv):
         self._tv = tv
